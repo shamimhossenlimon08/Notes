312d05bd515f3407ecf00a3cf0b574e80647b3a1
Shamim Hossen Limon <shamimhossenlimon088@gmail.com>
2025-02-08 15:44:41 +0600

    logo-path-3rd

--- src/components/menubar/index.jsx
+++ src/components/menubar/index.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           <div className="pl-1 pt-1">
             <a href="#">
               <img
-                src="./image/logo.png"
+                src="image/logo.png"
                 alt="notes Image"
                 className="h-10 w-12 bg-lime-500 pl-2 rounded-full shrink-0"
               />
